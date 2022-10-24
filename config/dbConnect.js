be23e20834db8acdcaa5362ec46da792018a2662
JugolKarmakar <jugol.cse@gmail.com>
2022-10-24 23:51:54 +0600

    delpoy

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsszmfv.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsszmfv.mongodb.net/acc-server?retryWrites=true&w=majority`;
 
 mongoose
   .connect(uri)
